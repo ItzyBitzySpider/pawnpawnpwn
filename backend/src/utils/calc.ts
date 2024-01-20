fac9b5a60fe2d268df6f03bfcdddcc2c0f04e99e
Jonathan Loh <jonathanloh02@gmail.com>
2024-01-20 21:12:12 +0800

    Revert room code generation to characters

--- backend/src/utils/calc.ts
+++ backend/src/utils/calc.ts
@@ -1,4 +1,11 @@
 // Generate a random 6-letter room code
 export function generateRoomCode() {
-  return Math.random().toString(36).substring(2, 8).toUpperCase();
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let roomCode = "";
+  for (let i = 0; i < 6; i++) {
+    roomCode += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+  return roomCode;
 }
